4e212fa7776107a9a9aa150e11c06fe6b3849bd9
davidbetteridge <david.betteridge@zeromission.io>
2024-12-03 22:23:17 +0000

    more

--- dotnet_examples/CSharp/1_NominalTyping.cs
+++ dotnet_examples/CSharp/1_NominalTyping.cs
@@ -13,3 +13,17 @@ class C2
 
 // Same structure,  but cannot be use interchangeably
 
+class Test
+{
+    public Test()
+    {
+        f(new C1());
+        f(new C2());
+    }
+    
+    void f(C1 obj)
+    {
+        Console.WriteLine(obj.i);
+    }
+    
+}
